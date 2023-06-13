79eadef3898031b211425842ed0f8c32caa3e213
monbrize <monbrize@gmail.com>
2023-06-13 15:27:50 +0900

    first commit in STS - create project

---
+++ CafeDukku/src/main/java/com/caduk/demo/CafeDukkuApplication.java
@@ -0,0 +1,13 @@
+package com.caduk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeDukkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeDukkuApplication.class, args);
+	}
+
+}
---
+++ CafeDukku/src/test/java/com/caduk/demo/CafeDukkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caduk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeDukkuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
