facad8d99ee979fd6cc48209930d968b2048a222
monbrize <monbrize@gmail.com>
2023-06-16 15:32:11 +0900

    기본 환경 셋팅(1) 버전 변경, 서버 테스트 (Port:8002)

--- CafeDukku/src/main/java/com/caduk/demo/CafeDukkuApplication.java
+++ CafeDukku/src/main/java/com/caduk/demo/CafeDukkuApplication.java
@@ -2,8 +2,10 @@ package com.caduk.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.caduk"})
 public class CafeDukkuApplication {
 
 	public static void main(String[] args) {
